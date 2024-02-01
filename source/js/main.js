3dc3f6dfc7e97915df285c9bb6f3e2ba6c423d39
nikolay tuvykin <tuvykin93@yandex.ru>
2024-02-01 11:26:11 +0300

    сохранился на форме

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import './utils/scroll-lock';
 import {initHeroSlider, initProgramsSlider, initReviewsSlider, initNewsSlider} from './modules/init-swiper';
 import './vendor/accordion';
+import { CustomSelect } from './select/custom-select';
 
 // ---------------------------------
 
@@ -22,9 +23,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  // window.addEventListener('load', () => {
-  //   const form = new Form();
-  //   window.form = form;
-  //   form.init();
-  // });
+  window.addEventListener('load', () => {
+    // const form = new Form();
+    // window.form = form;
+    // form.init();
+  });
 });
