f44d177c76a08bd99042b9aecc1f15c94d9002b4
nikolay tuvykin <tuvykin93@yandex.ru>
2024-02-06 16:36:19 +0300

    добавляет состояния до формы

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import { Form } from './modules/form-validate/form';
 import {initHeroSlider, initProgramsSlider, initReviewsSlider, initNewsSlider} from './modules/init-swiper';
 import './vendor/accordion';
 import { CustomSelect } from './modules/select/custom-select';
@@ -25,9 +26,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // const form = new Form();
-    // window.form = form;
-    // form.init();
+    const form = new Form();
+    window.form = form;
+    form.init();
     const select = new CustomSelect();
     select.init();
     const header = new StickyHeader();
